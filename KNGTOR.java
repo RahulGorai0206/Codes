1907134c96afe538d6b493679bb29c488960543b
RahulGorai0206 <rahulgorai0206@gmail.com>
2023-03-02 21:26:09 +0530

    Tour of King
    Problem
    King loves to go on tours with his friends.
    King has N cars that can seat 5 people each and M cars that can seat 7 people each. Determine the
    maximum number of people that can travel together in these cars.
    Input Format
    The first line of input contains a single integer T, the number of test cases.
    The first and only line of each test case contains two space-separated integers N and M — the
    number of 5-seaters and 7-seaters, respectively.
    Output Format
    For each test case, output on a new line the maximum number of people that can travel together.
    Constraints
    • T < 100
    • N, M < 100
    
    Explanation:
    Test case 1: King has 4 cars that seat 5 each and 8 cars that seat 7 each. So, 4 x 5 + 8 X 7 = 76
    people can travel together.
    Test case 2: King has 2 cars that seat 5 each and 13 cars that seat 7 each. So, 2 x 5 + 13 x 7 = 101
    people can travel together.
    Test case 3: King has 14 cars that seat 5 each and 5 cars that seat 7 each. So, 14 x 5 + 7 x 7 = 105
    people can travel together.
    Test case 4: King has 8 cars that seat 5 each and 8 cars that seat 7 each. So, 8 x 5 + 8 x 7 = 96
    people can travel together.

---
+++ KNGTOR.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int X=0,Y=0,count=0;
+	    Scanner scan = new Scanner(System.in);
+	    count=scan.nextInt();
+	    for (int i=0; i<count; i++){
+    		X=scan.nextInt();
+    		Y=scan.nextInt();
+		System.out.println((X*5)+(Y*7));
+	    }
+	}
+}
